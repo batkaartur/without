0d92c38ed64f472f0bd6864145fa9df402cd3462
in-hale <whitelion98@gmail.com>
2017-12-06 23:45:54 +0300

    added news adding function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,4 +9,10 @@ class WelcomeController < ApplicationController
     send_file("#{Rails.root}/public/files/#{params[:file_name]}")
   end
 
+  def addnews 
+  	Post.new(:image => 'https://pp.userapi.com/c834301/v834301119/4a0ee/d7nw02WwVf8.jpg', :title => 'Всем мальчикам скидка на протяжении года 10%', :body => 'Всем мальчикам скидка на протяжении года 10%').save!
+  	Post.new(:image => 'https://pp.userapi.com/c841121/v841121119/44126/jWgz51f-Z3U.jpg', :title => 'Открыт новый набор на группы от 5 лет с января 2018', :body => 'Открыт новый набор на группы от 5 лет с января 2018').save!
+  	Post.new(:image => 'https://pp.userapi.com/c841121/v841121119/44126/jWgz51f-Z3U.jpg', :title => 'Открыт новый набор на группы от 5 лет с января 2018', :body => 'Открыт новый набор на группы от 5 лет с января 2018').save!
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
   root 'welcome#index'
   get 'welcome/index'
+  get 'addnews', to: 'welcome#addnews'
   post 'welcome/download'
 end
